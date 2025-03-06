a54f36d45c2eb1b104f4b5f3dcff52f84eca577d
dronperminov <perminov@ispras.ru>
2025-03-06 21:25:23 +0300

    add own axes select for synthetic data experiment

--- js/playground/experiments.js
+++ js/playground/experiments.js
@@ -1,3 +1,13 @@
+Playground.prototype.BuildAxisSelect = function(dimension, axis, parent = null) {
+    let select = MakeElement(parent, {class: "basic-input inline-input"}, "select")
+
+    for (let i = 0; i < dimension; i++)
+        MakeElement(select, {value: i, innerText: `x${NumberToIndex(i + 1)}`}, "option")
+
+    select.value = axis
+    return select
+}
+
 Playground.prototype.RunSyntheticDataExperiment = function() {
     let count = this.syntheticDataBackgroundCount.GetValue()
     let data = this.visualizer.GenerateSyntheticData(count)
@@ -12,16 +22,23 @@ Playground.prototype.RunSyntheticDataExperiment = function() {
     MakeElement(stepsList, {class: "text", innerHTML: "Для каждой сгенерированной точки x<sub>i</sub> была получено предсказание модели y<sub>i</sub>"}, "li")
 
     if (low < high)
-        MakeElement(stepsList, {class: "text", innerHTML: `Точки ${low} &le; y<sub>i</sub> &lt; ${high} были удалены (пороги доверия)`}, "li")
+        MakeElement(stepsList, {class: "text", innerHTML: `Точки с ${low} &le; y<sub>i</sub> &lt; ${high} были удалены (пороги доверия)`}, "li")
 
-    MakeElement(stepsList, {class: "text", innerHTML: "Точка x<sub>i</sub> с вероятностью |y<sub>i</sub>| была отобрана в качестве синтетической"}, "li")
+    MakeElement(stepsList, {class: "text", innerHTML: "В качестве синтетической выборки были выбраны точки x<sub>i</sub> с вероятностью |y<sub>i</sub>|"}, "li")
 
     if (data.length == 0) {
         MakeElement(stepsList, {class: "text", innerText: "В результате все сгенерированные точки были отвергнуты"}, "li")
         return
     }
 
-    MakeElement(stepsList, {class: "text", innerHTML: `В результате были отобраны <b>${data.length}</b> точек (${Round(data.length * 100 / count, 10)}%):`}, "li")
+    MakeElement(stepsList, {class: "text", innerHTML: `В результате были отобраны <b>${data.length}</b> точек (${Round(data.length * 100 / count, 10)}%)`}, "li")
+
+    let axisList = MakeElement(this.tabExperiments, null, "ul")
+    let liX = MakeElement(axisList, {innerHTML: "Проекционная координата на <b>ось X</b>: "}, "li")
+    let liY = MakeElement(axisList, {innerHTML: "Проекционная координата на <b>ось Y</b>: "}, "li")
+
+    let axisX = this.BuildAxisSelect(data.dimension, this.axisX.value, liX)
+    let axisY = this.BuildAxisSelect(data.dimension, this.axisY.value, liY)
 
     let dataPlotDiv = MakeElement(this.tabExperiments, {class: "data-plot", style: "width: 300px; height: 300px;"})
     let dataPlotSVG = MakeElement(dataPlotDiv, null, "svg")
@@ -32,7 +49,10 @@ Playground.prototype.RunSyntheticDataExperiment = function() {
 
     let dataPlot = new DataPlot(dataPlotDiv, viewBox, this.visualizer.compact)
     dataPlot.AddPlot("synthetic", {border: "#ffffff", colors: ["#2191fb", "#89dd73", "#dd7373"], visible: true})
-    dataPlot.SetAxes(+this.axisX.value, +this.axisY.value)
+    dataPlot.SetAxes(+axisX.value, +axisY.value)
+
+    axisX.addEventListener("change", () => dataPlot.SetAxes(+axisX.value, +axisY.value))
+    axisY.addEventListener("change", () => dataPlot.SetAxes(+axisX.value, +axisY.value))
 
     let dataTable = new DataTable(dataTableDiv)
     dataTable.Add("synthetic", {title: "Синтетические данные", colors: {"-1": "#2191fb", "1": "#dd7373"}})
